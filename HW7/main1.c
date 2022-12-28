39a36c1fd9662eae84c75e9b25f25aa8573bbbfc
larry487 <119911822+larry487@users.noreply.github.com>
2022-12-29 00:25:43 +0800

    Create main1.c

---
+++ HW7/main1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   int n =7;
+   int spc = n;
+   
+   for (int i=1;i<=n;i++){
+           printf("%*c",n-i+1,' ');
+       for(int j=1;j<=i;j++){
+           printf(" ");
+           printf("%d",i);
+       }
+       printf("\n");
+       spc--;
+   }
+}
