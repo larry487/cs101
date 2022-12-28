fa05ac7b133ba5bb1bf7a0416ead0b242f2f19f6
larry487 <119911822+larry487@users.noreply.github.com>
2022-12-28 10:24:32 +0800

    Update main2.c

--- HW6/main2.c
+++ HW6/main2.c
@@ -5,9 +5,9 @@ int main()
     int i = 10;
 
     if(i %2 == 0)
-        printf("%d even", i);
+        printf("%d even");
     else
-        printf("%d odd", i);
+        printf("%d odd");
  
     return 0;
 }
